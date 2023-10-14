5f5c97ef404a92e302ea0c258dd967cda61b6154
aerichs <115708302+aerichs@users.noreply.github.com>
2023-10-14 16:16:38 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,13 +12,3 @@ var typed = new Typed(".aboutMe", {
   startDelay: 1000,
 });
 
-function hoho() {
-  Swal.fire({
-    title: "Her er ett tilfeldig bilde!",
-    text: "Fint, ikke sant?",
-    imageUrl: "https://unsplash.it/400/200",
-    imageWidth: 400,
-    imageHeight: 200,
-    imageAlt: "Custom image",
-  });
-}
